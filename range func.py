5cc4e22781371b2437b8ed5a8e2ffda9bfb73446
shravanisy2006 <shravanisy2000@gmail.com>
2026-05-29 17:10:24 +0530

    Add range-related code examples

---
+++ range func.py
@@ -0,0 +1,18 @@
+range related code:
+
+for i in range (100,0,-2):
+    print(i)
+
+
+for i in range (1,6):
+    print("*"*i)
+
+for i in range(1,6):
+    print("Hellow world!")
+
+for i in range(1,6):
+    print(i)
+
+n=int(input("Enter any number:"))
+for i in range(n):
+    print("Hello World!")
